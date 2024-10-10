adf582b02657f6b4bdeb867120d17efc88a7a9cd
ambidextrous9 <sushovansaha95@gmail.com>
2024-10-10 19:12:08 +0530

    T

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 import config
 import time
 import warnings
-# from getModels import model_init,model_details
+from getModels import model_init,model_details
 from transform import transform_norm
 warnings.filterwarnings("ignore")
 
@@ -18,43 +18,43 @@ st.set_page_config(page_title="ambideXtrous",
 index_to_class = config.INV_LABELS_CLASS
 
 
-# models = model_init()
+models = model_init()
 
 
-# def inference(cnnmodel,image_path):
+def inference(cnnmodel,image_path):
     
-#     if(cnnmodel[1] != None):
+    if(cnnmodel[1] != None):
        
-#         image = Image.open(image_path).convert('RGB')
+        image = Image.open(image_path).convert('RGB')
         
-#         cnnmodel[1].eval()
-#         input_tensor = transform_norm(image).unsqueeze(0)  # Add batch dimension
+        cnnmodel[1].eval()
+        input_tensor = transform_norm(image).unsqueeze(0)  # Add batch dimension
         
-#         # Measure inference time
-#         start_time = time.time()
+        # Measure inference time
+        start_time = time.time()
         
-#         with torch.no_grad():
-#             output = cnnmodel[1](input_tensor)
-#             probs = torch.exp(output)
-#             # Get the predicted class index
-#             index = torch.argmax(output).item()
+        with torch.no_grad():
+            output = cnnmodel[1](input_tensor)
+            probs = torch.exp(output)
+            # Get the predicted class index
+            index = torch.argmax(output).item()
             
-#         end_time = time.time()
-#         inference_time = end_time - start_time
+        end_time = time.time()
+        inference_time = end_time - start_time
             
-#         predicted_class = index_to_class[index]
-#         predprob = round(probs[0][index].item(),2)
-#         if(predprob < 0.80) : predicted_class = 'None'
+        predicted_class = index_to_class[index]
+        predprob = round(probs[0][index].item(),2)
+        if(predprob < 0.80) : predicted_class = 'None'
         
-#         st.markdown(f"🚀 Model: **{cnnmodel[0]}**")
-#         model_details(cnnmodel[1])
-#         st.markdown(f"🌱 Predicted Class: **{predicted_class}**")
-#         st.markdown(f"🎃 Accuracy: **{predprob:.2f}**")
-#         st.markdown(f"🌟 Inference Time: **{inference_time:.4f} seconds**")
+        st.markdown(f"🚀 Model: **{cnnmodel[0]}**")
+        model_details(cnnmodel[1])
+        st.markdown(f"🌱 Predicted Class: **{predicted_class}**")
+        st.markdown(f"🎃 Accuracy: **{predprob:.2f}**")
+        st.markdown(f"🌟 Inference Time: **{inference_time:.4f} seconds**")
             
-#     else:
-#         st.markdown(f"🚀 Model: **{cnnmodel[0]}**")
-#         st.markdown(f"☠️ No Model in Production..!!**")
+    else:
+        st.markdown(f"🚀 Model: **{cnnmodel[0]}**")
+        st.markdown(f"☠️ No Model in Production..!!**")
 
         
 
@@ -70,23 +70,23 @@ def model_card():
         col1, col2, col3, col4 = st.columns(4)
 
         # Card 1: Model Name
-        # with col1:
-        #     inference(models[0],image_path=imgpath)
+        with col1:
+            inference(models[0],image_path=imgpath)
         
 
-        # # Card 2: Model Details
-        # with col2:
-        #     inference(models[1],image_path=imgpath)
+        # Card 2: Model Details
+        with col2:
+            inference(models[1],image_path=imgpath)
             
 
-        # # Card 3: Predicted Class
-        # with col3:
-        #     inference(models[2],image_path=imgpath)
+        # Card 3: Predicted Class
+        with col3:
+            inference(models[2],image_path=imgpath)
             
 
-        # # Card 4: Inference Time
-        # with col4:
-        #     inference(models[3],image_path=imgpath)
+        # Card 4: Inference Time
+        with col4:
+            inference(models[3],image_path=imgpath)
     
     st.write("""
     ### 🚀 **Exploring Image Classification Models with Transfer Learning!**
