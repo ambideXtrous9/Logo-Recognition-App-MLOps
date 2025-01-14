564ba3191b12837fc4f8f9830f0bf6e56b626fb1
Sushovan Saha <31372586+ambideXtrous9@users.noreply.github.com>
2025-01-14 16:38:41 +0530

    modelCard updated

--- modelCard.py
+++ modelCard.py
@@ -10,7 +10,7 @@ def model_card(models):
     
     if imgpath is not None:
         
-        st.image(imgpath, caption="Uploaded Image", use_column_width=True)
+        st.image(imgpath, caption="Uploaded Image", use_container_width=True)
 
         col1, col2, col3, col4 = st.columns(4)
 
